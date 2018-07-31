f207a36a2784711c08d05a76d6180968256a09ca
Igor <igor_pavlenko@yahoo.com>
2018-07-31 20:50:11 +0300

    oo, created samll book app with oo

--- 3_2_project_files/Objectoriented/objectsspecialist.js
+++ 3_2_project_files/Objectoriented/objectsspecialist.js
@@ -1,11 +1,4 @@
-function User(name, age) {
-    this.name = name;
-    this.age = age;
-    this.admin = false;
-}
 
-let u1 = new User('Archie', 25);
-let u2 = new User('Reem', 20);
 
 // console.log(u1.constructor, u2.constructor); // we can check who is the let construcotor
 
@@ -51,3 +44,101 @@ let u2 = new User('Reem', 20);
         /accepted/.test(email);
     
 */
+
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+    this.admin = false;
+}
+
+let u1 = new User('Archie', 2);
+let u2 = new User('Reem', 25);
+User.prototype.x = 10;
+
+// if we want to add any prototype to the object we noramlly do it outside the object 
+User.prototype.say = function (w){
+    console.log(`${this.name} ${w}`);
+    console.log(this.name +' '+ w);
+}
+
+u1.say('Hello');
+
+
+// how to include unlimited # of passed values into array with funciton  
+/*
+    array has also constructor 
+    1 - call prototype on array 
+    2 - slice method with slice all passed arguments into array
+    3 - call - calling the function with specific this arguments 
+    4 - arguments - are all of the passed arguments into the fuctinon
+    4 - will output all the arguments passed (2) ["igor", "reem"]
+*/
+function func () {
+    return Array.prototype.slice.call(arguments);
+}
+
+console.log(func('igor', 'reem'));
+console.log(func('i','g','o','r'));
+
+
+
+// add other methods to 
+
+Number.prototype.pow = function (x){
+    return Math.pow(this.valueOf(), x);
+}
+
+/* 
+    as we know all the objects follow the properties of global object 
+    so we can reassign or add properties to the global Object 
+
+    example of the possible issue that this may couse 
+
+    we need to loop through own properties 
+
+    for(let i in u){
+        if(u.hasOwnProperty(i)){
+            console.log(u);
+        }
+    }
+*/
+
+
+/*
+    also important to check if the let is an instance of object or array as both of them are objects 
+    we can check it in the following way 
+
+    [] instanceof Object
+    true
+    [] instanceof Array
+    true
+    let i = {}
+    undefined
+    i instanceof Object
+    true
+    i instanceof Array
+    false
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 3_2_project_files/Objectoriented/personalchallenges.js
+++ 3_2_project_files/Objectoriented/personalchallenges.js
@@ -1,10 +1,13 @@
+// validate email
+
 const emailSubmitButton = document.getElementById('em-subm-btn');
 const emailInput = document.getElementById('email-field');
 const emailArray = [];
 const passing = /[a-z]*/;
 // add event listeners 
 emailSubmitButton.addEventListener('click', getValue);
-    let counter = 0;
+let counter = 0;
+
 function getValue(e) {
     e.preventDefault();
     let i = emailInput.value;
@@ -13,18 +16,80 @@ function getValue(e) {
     // test the email values
     let re = new RegExp(emailArray.join('|', i));
     console.log(re.test(passing));
-    
-        if (re.test(passing)) {
-            let li = document.createElement('li');
-            li.textContent = emailArray[counter];
-            document.getElementById('passed').appendChild(li);
-        }
-    
+
+    if (re.test(passing)) {
+        let li = document.createElement('li');
+        li.textContent = emailArray[counter];
+        document.getElementById('passed').appendChild(li);
+    }
+
 
 
 
     console.log(emailArray);
-    counter ++;
+    counter++;
 }
 
 console.log(emailSubmitButton, emailInput);
+
+
+
+// Book construcor 
+
+// create object book 
+// append books to the li 
+// add button to each book to show the price 
+
+const bookList = document.getElementById('book-list');
+const addBookBtn = document.getElementById('add-book');
+let button = document.createElement('button');
+
+function Book(title, author, price) {
+    this.title = title;
+    this.author = author;
+    this.price = price;
+}
+
+Book.prototype.addBookToLi = function () {
+    let li = document.createElement('li');
+    li.textContent =`Book ${this.title} with the title: ${this.author}` ;
+    bookList.appendChild(li);
+}
+
+Book.prototype.addPriceButton = function (){
+    button.textContent = 'Show the price';
+    button.className = 'show-price';
+    document.getElementById('book-list').appendChild(button);
+}
+
+Book.prototype.showPrice = function(){
+    alert(this.price);
+}
+
+const book1 = new Book('Archie', 'Happy in Juffair', 'BD500');
+
+function addBook() {
+    book1.addBookToLi();
+    book1.addPriceButton();
+}
+
+function showThePrice (){
+    book1.showPrice();
+}
+
+// add Event listnters 
+addBookBtn.addEventListener('click', addBook);
+button.addEventListener('click', showThePrice);
+
+
+
+console.log(bookList);
+
+
+
+
+
+
+
+
+
