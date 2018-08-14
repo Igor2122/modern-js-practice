3e838060018c13338ba0b0aeb0c69b8593a8d73a
Igor <igor_pavlenko@yahoo.com>
2018-08-14 22:10:20 +0300

    book extending app finished

---
+++ BOM/labs/app.js
@@ -0,0 +1,44 @@
+window.onload = setPlusIcon;
+
+
+function setPlusIcon() {
+  let allLi = document.getElementsByTagName('li');
+  for (let i = 0; i < allLi.length; i++) {
+    let li = allLi[i];
+    let allUl = li.getElementsByTagName('ul');
+    if (allUl.length > 0) {
+      if (allUl[0].className == 'expandable') {
+        li.style.listStyleImage = 'url("img/plus.gif")';
+      }
+    }
+  }
+}
+
+let links = document.getElementsByTagName('a');
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', aClick, false);
+}
+
+function aClick(e) {
+  e = e || event;
+
+  let clicked = e.target.parentNode || e.srcElement;
+  let li = clicked.parentNode;
+  console.log(clicked);
+  let ul = clicked.getElementsByTagName('ul');
+  if (ul.length == 0) {
+    return true;
+  }
+  if (ul[0].style.display == 'block') {
+    ul[0].style.display = '';
+    li.style.listStyleImage = 'url("img/plus.gif")';
+  } else {
+    ul[0].style.display = 'block';
+    li.style.listStyleImage = 'url("img/point.gif")';
+  }
+  try {
+    e.preventDefault();
+  } catch (x) {
+    e.returnValue = false;
+  }
+}
