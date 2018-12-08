936222660b47d88ce8e02462896792e91fd2e41b
Igor <igor_pavlenko@yahoo.com>
2018-12-08 18:46:11 +0000

    practicing solving algrtm challenges

--- freecodecamp/bscalgritms.js
+++ freecodecamp/bscalgritms.js
@@ -1,14 +1,105 @@
+// Converting Celcies to Farenheit 
 function convertToF(celsius) {
   let fahrenheit = celsius * 9/5 + 32;
   return fahrenheit;
 }
+// console.log(convertToF(30));
 
-console.log(convertToF(30));
 
+// Reversing the string
 function reverseString(str) {
-  let res = str.split(', ');
-  console.log(res);
-  return str;
+  let res = str.split('');
+  res.reverse();
+  let reversedString = res.join('');
+  console.log(reversedString);
+  return res;
 }
 
-console.log(reverseString("hello"));
+// console.log(reverseString("hello"));
+
+// Factorialize a Number
+function factorialize(num) {
+  let res = null;
+  if(num == 0){
+    return 1;
+  }
+  let val = 0
+  let arr = [];
+  while(val < num) {
+    val ++;
+    arr.push(val);
+  }
+  res = arr.reduce((a, b) => a * b);
+  console.log(res);
+  return res;
+}
+
+//  console.log(factorialize(0));
+
+// Find the Longest Word in a String
+function findLongestWordLength(str) {
+  let strArr = str.split(' ');
+  let arr = []
+  strArr.map(length => {
+    arr.push(length.length);
+  })
+  let longestStr = (Math.max(...arr));
+  return longestStr;
+}
+
+// console.log(findLongestWordLength("May the force be with you"));
+
+// Return Largest Numbers in Arrays
+function largestOfFour(arr) {
+  // You can do this!
+  let numarr = [];
+  arr.map(res => {
+  let  MaxNum = (Math.max(...res));
+      numarr.push(MaxNum)
+  })  
+  return numarr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// Algorithm Scripting: Confirm the Ending
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  let length = target.length;
+  let tstedStr =  str.split(' ');
+  let lastElem = tstedStr.pop()
+  let formattedString = lastElem.substring(lastElem.length - length);
+  
+  console.log(formattedString, lastElem.length - length);
+  
+  if(formattedString == target){
+    return true;
+  } else {
+    return false;
+  }
+
+}
+// let string = 'Abstraction';
+// console.log(string.substring(5));
+
+// console.log(confirmEnding("Bastian", "n"));
+// console.log(confirmEnding("He has to give me a new name", "name"));
+// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Abstraction", "action"));
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
